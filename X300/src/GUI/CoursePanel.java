50a32ee545feb90219f22657947dfcfaa4696c9c
Naomi Espinosa <naomisespinosa@gmail.com>
2019-09-17 11:56:56 +0200

    Designed GUI for the small course panel and added GUI components and corresponding fixIds to class

--- X300/src/GUI/CoursePanel.java
+++ X300/src/GUI/CoursePanel.java
@@ -1,4 +1,29 @@
 package GUI;
 
-public class CoursePanel {
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+
+public class CoursePanel extends AnchorPane {
+
+    @FXML
+    private ImageView courseImage;
+
+    @FXML
+    private Text courseNameLabel;
+
+
+    @FXML
+    public Image getCourseImage(String course) {
+        String iconPath;
+        Image coursePic = new javafx.scene.image.Image(getClass().getClassLoader().getResourceAsStream(iconPath));
+        iconPath = "RecipeSearch/resources/icon_flag_sweden.png";
+        return coursePic;
+    }
+
+
+
 }
