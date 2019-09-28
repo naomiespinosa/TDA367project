c6d551a88e78da25d916809671c6ef69894b274b
Robin Hallabro-Kokko <robin@hallabro.nu>
2019-09-28 17:43:51 +0200

    implement a basic dependency injection module
    This also renames MainController into Min5aApplication, as the previous
    controller was not really a controller.

---
+++ src/main/java/DependencyInjectionModule.java
@@ -0,0 +1,8 @@
+import com.google.inject.AbstractModule;
+import model.timer.TimerManager;
+
+public class DependencyInjectionModule extends AbstractModule {
+  @Override
+  protected void configure() {
+  }
+}
---
+++ src/main/java/Min5aApplication.java
@@ -0,0 +1,30 @@
+import com.cathive.fx.guice.GuiceApplication;
+import com.cathive.fx.guice.GuiceFXMLLoader;
+import com.google.inject.Inject;
+import com.google.inject.Module;
+import java.io.IOException;
+import java.util.List;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Min5aApplication extends GuiceApplication {
+  @Inject private GuiceFXMLLoader fxmlLoader;
+
+  @Override
+  public void init(final List<Module> modules) throws Exception {
+    modules.add(new DependencyInjectionModule());
+  }
+
+  @Override
+  public void start(Stage stage) throws IOException {
+    Parent root =
+        fxmlLoader
+            .load(getClass().getClassLoader().getResource("fxml/Side_Panel.fxml"))
+            .getRoot();
+    Scene scene = new Scene(root);
+    stage.setTitle("Min5a");
+    stage.setScene(scene);
+    stage.show();
+  }
+}
