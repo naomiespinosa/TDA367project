98c145e1c367d60f6f7464c30ed29f59488b1f30
Hanna <hannatarnasen@hotmail.com>
2019-10-01 10:48:08 +0200

    Have completed the functionality of the sidePanel
    All buttons now function and the app initialises with the coursePage on top. Instead of loading a controller with and fxml it now loads an fxml file with an controller. A factory has been implemented to further the struction and abstraction between the modules. It is now cleanly build and open to extension. The application now created three pages that are put in the controller and are obtained in the sidePanel that decides witch ones are to be shown.
    
    Also some classes have been renamed to fit better

--- src/main/java/controller/CoursePanelItem.java
+++ src/main/java/controller/CoursePanelItem.java
@@ -1,31 +1,12 @@
 package controller;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
-public class CoursePanelItem extends AnchorPane {
-  private SidePanel parentController;
+public class CoursePanelItem {
 
   @FXML private ImageView courseImageView;
 
   @FXML private Text courseNameLabel;
-
-  // CONSTRUCTOR FOR COURSE PANEL ITEM
-  public CoursePanelItem(SidePanel controller) {
-    FXMLLoader fxmlLoader = new FXMLLoader(CoursePanelItem.class.getClassLoader().getResource("fxml/CoursePanelItem.fxml"));
-    fxmlLoader.setRoot(this);
-    fxmlLoader.setController(this);
-
-    try {
-      fxmlLoader.load();
-    } catch (IOException exception) {
-      throw new RuntimeException(exception);
-    }
-
-    this.parentController = controller;
-  }
 }
--- src/main/java/controller/SidePanel.java
+++ src/main/java/controller/SidePanel.java
@@ -1,23 +1,60 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
-public class SidePanel {
-
-  @FXML private Button escapeHatchButton;
-
-  @FXML private Button coursesPageButton;
-
-  @FXML private Button statisticsPageButton;
+public class SidePanel implements Initializable {
 
   @FXML private AnchorPane mainPage;
 
-  public void homePage(javafx.event.ActionEvent actionEvent) {
+  // Other FXMLs
+  private AnchorPane homePage;
+  private AnchorPane courseSelectionPage;
+  private AnchorPane statisticsPage;
+
+  // Other FXML setters
+  public void setHomePage(AnchorPane homePage) { this.homePage = homePage; }
+
+  public void setCourseSelectionPage(AnchorPane courseSelectionPage) {
+    this.courseSelectionPage = courseSelectionPage;
+  }
+
+  public void setStatisticsPage(AnchorPane statisticsPage) {
+    this.statisticsPage = statisticsPage;
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+  }
+
+  public void init(){
+    showPage(courseSelectionPage);
+  }
+
+  @FXML
+  public void showHomePage(ActionEvent actionEvent) {
+    showPage(homePage);
+  }
+
+  @FXML
+  void showCourseSelectionPage(ActionEvent event) {
+    showPage(courseSelectionPage);
+  }
+
+  @FXML
+  void showStatisticPage(ActionEvent event) {
+    showPage(statisticsPage);
+  }
+
+  // Shows selected page on the right side of the screen
+  private void showPage(AnchorPane page) {
     mainPage.getChildren().clear();
-    CourseOverviewPage courseOverviewPage = new CourseOverviewPage(this);
-    mainPage.getChildren().add(courseOverviewPage);
+    mainPage.getChildren().add(page);
     mainPage.toFront();
   }
 }
