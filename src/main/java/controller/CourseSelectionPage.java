409324917b353b2dd0278534b83a0ede2ed3dcbf
johannawiberg <johanna440@live.se>
2019-10-02 17:06:42 +0200

    New sortCourses method. Committing only because im working with Hanna.

--- src/main/java/controller/CourseSelectionPage.java
+++ src/main/java/controller/CourseSelectionPage.java
@@ -19,9 +19,9 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class CourseSelectionPage implements Initializable{
-    private List<Course> activeCourses;
-    private List<Course> inactiveCourses;
-    private List<Course> allCourses;
+    private List<Course> activeCourses = new ArrayList<>();
+    private List<Course> inactiveCourses = new ArrayList<>();
+
     public CourseSelectionPage coursePanelItemCtrl;
 
     @FXML
@@ -65,92 +65,28 @@ public class CourseSelectionPage implements Initializable{
     }
 
     private void showActiveCourses() {
-        activeCourses = getActiveCourses();
     }
 
     private void showInactiveCourses() {
-        inactiveCourses = getInactiveCourses();
-
     }
 
-    private List<Course> getActiveCourses() {
-        return sortCourses(true);
-    }
 
-    private List<Course> getInactiveCourses(){
-        return sortCourses(false);
-    }
 
-    public List<Course> sortCourses(Boolean status){
-        List<Course> tempCourses = new ArrayList<>();
+    public void sortCourses(){
         for (int i = 0; i < user.getCourses().size();i++){
-            if (user.getCourse(i).isActive() == status){
-                tempCourses.add(user.getCourse(i));
+            if (user.getCourse(i).isActive()){
+                activeCourses.add(user.getCourse(i));
             }
-        }
-        return tempCourses;
+            else{
+                inactiveCourses.add(user.getCourse(i));
+            }
+
     }
 
 
 
-   /* public void showCourseItem() throws IOException {
-        FXMLLoader courseSelectionLoader =
-                new FXMLLoader(getClass().getClassLoader().getResource("fxml/CoursePanelItem.fxml"));Parent root2 = courseSelectionLoader.load(); // Loads the FXML for the Panel
-        CourseSelectionPage courseSelectionCtrl =
-                courseSelectionLoader.getController(); // Fetches the Controller for the fxml
 
-        Course testCourse1 = new Course("testtes3t", "TEST1323",2019,4);
-
-        Course testCourse = new Course("testtest", "TEST123",2019,4);
-        CoursePanelItem item = new CoursePanelItem(testCourse, courseSelectionCtrl);
-        CoursePanelItem item2 = new CoursePanelItem(testCourse1, courseSelectionCtrl);
-       // activeCoursesFlowpane.getChildren().add(item);
-       // activeCoursesFlowpane.getChildren().add(item2);
-
-
-    }*/
-
-
-
-    public void setActiveCoursePanelItem(FlowPane activeCoursesFlowpane) {
-        this.activeCoursesFlowpane = activeCoursesFlowpane;
+    }
     }
 
-    public void setinactiveCoursePanelItem(FlowPane inactiveCoursesFlowpane) {
-        this.inactiveCoursesFlowpane = inactiveCoursesFlowpane;
-    }
-    public void setCoursePanelItem(AnchorPane smallCoursePanelItem )
-    { this.smallCoursePanelItem = smallCoursePanelItem; }
 
-
-   public void updateSelectedCourses(List<Course> activeCourses) throws IOException {
-       activeCoursesFlowpane.getChildren().clear();
-       int i;
-       for (i = 0; i <= activeCourses.size() - 1; i++){
-        FXMLLoader coursePanelItemLoader =
-               new FXMLLoader(getClass().getClassLoader().getResource("fxml/CoursePanelItem.fxml"));
-       Parent root = coursePanelItemLoader.load();
-       CourseSelectionPage coursePanelItemCtrl = coursePanelItemLoader.getController();
-           setCoursePanelItem(PageFactory.createCoursePanelItem());
-             // activeCoursesFlowpane.getChildren().add(activeCourses.get(i));
-
-           }
-       }
-
-
-
-
-
-
-    /*
-     @FXML
-    void showCourseSelectionPage(ActionEvent event) {
-        showPage(CourseMainPage);
-    }
-
-    private void showPage(FlowPane page) {
-        mainPage.getChildren().clear();
-        mainPage.getChildren().add(page);
-        mainPage.toFront();
-    }*/
-}
