8688ed0c82afba29a06a72b84c90026c234f7eb7
Robin Hallabro-Kokko <robin@hallabro.nu>
2019-10-14 19:21:29 +0200

    drop obsolete tests

--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -6,23 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
   @Test
-  public void courseFunctions() {
-    User user = new User();
-    user.clearCourses();
-    user.addCourse("Funktionell Programmering", "TDA333", 1, 2);
-    user.addCourse("Programmering", "TDA333", 1, 2);
-    user.addCourse("Mattematisk Analys", "TDA333", 1, 2);
-    user.getCourse(2).endCourse("4");
-    user.addCourse("Kommunikation och ingejörskunskap", "TDA333", 1, 2);
-    user.addCourse("Hej", "TDA333", 1, 2);
-
-    assertSame(5, user.getCourses().size());
-    assertSame("Mattematisk Analys", user.getCourse(2).getName());
-    assertSame(false, user.getCourse(2).isActive());
-    assertSame(true, user.getCourse(1).isActive());
-  }
-
-  @Test
   public void setGetFunctions() {
     User user = new User();
 
