4af6338be575bcce02e87aea047947220f48ad4e
Hanna <hannatarnasen@hotmail.com>
2019-10-24 11:36:22 +0200

    Took away kommented lines

--- src/test/java/model/CourseTest.java
+++ src/test/java/model/CourseTest.java
@@ -17,21 +17,6 @@ class CourseTest {
     model.login(TestData.number, TestData.pwd);
   }
 
-  /*
-    @Test
-    public void addCourseTest(){
-      model.addCourse("Programmering", "TDA367", 2019, 4);
-      model.addCourse("Någon annan Kurs", "TDA367", 2019, 4);
-
-      int counter = 0;
-      for (Course course : model.getCourses()) {
-        counter++;
-      }
-
-      assertSame(2, counter);
-    }
-  */
-
   @Test
   void whenCourseIsCreated() {
     Course course = new Course("Programmering", "TDA367", 2019, 4);
